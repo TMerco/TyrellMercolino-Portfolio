446415e80a40da96e93ad345cc96b53442335a65
TMerco <tmercolino@sea.yearup.org>
2023-03-20 17:23:06 -0700

    finished experience page

--- my-app/src/components/about/About.jsx
+++ my-app/src/components/about/About.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import './about.css'
 import ME from '../../assets/me-about.jpg'
+import {MdOutlineBusinessCenter} from 'react-icons/md'
+import {FiUsers} from 'react-icons/fi'
+import {MdOutlineLibraryAdd} from 'react-icons/md'
+
 const About = () => {
   return (
     <section id='about'>
@@ -15,7 +19,32 @@ const About = () => {
         </div>
 
         <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <MdOutlineBusinessCenter className='about__icon'/>
+              <h5>Experience</h5>
+              <small>1+ Years</small>
+            </article>
 
+            <article className='about__card'>
+              <FiUsers className='about__icon'/>
+              <h5>Network</h5>
+              <small>100+</small>
+            </article>
+
+            <article className='about__card'>
+              <MdOutlineLibraryAdd className='about__icon'/>
+              <h5>Projects</h5>
+              <small>5+ Completed</small>
+            </article>
+          </div>
+
+          <p className='about__paragraph1'>Hello my name is Tyrell Mercolino!</p>
+<p className='about__paragraph'>I am a Software Developer currently working for Alaska Airlines.</p>
+<p className='about__paragraph'> My previous job as a Maintenance Tech allowed me to network with people who were already developers. Through this I fell inlove with developing and began my journey into the tech field.
+I aspire to be a Mobile Developer and connect with others in our community.</p>
+          
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
         </div>
       </div>
     </section>
